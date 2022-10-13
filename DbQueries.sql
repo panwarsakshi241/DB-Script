d58307554b61768664f9775959ab8c6f4f92df03
sakshi panwar <sakshipanwar1912@gmail.com>
2022-10-13 12:58:37 +0530

    added the second query and comments

--- DbQueries.sql
+++ DbQueries.sql
@@ -1,1 +1,7 @@
- SELECT * from Account ORDER BY accountNumber Desc OFFSET 5  rows fetch Next 5 rows only;
+ --Query to fetch the chunk of data from Database using Offset and fetch next/first 
+ 
+ SELECT * from Account ORDER BY accountNumber Desc OFFSET 0 rows fetch Next 5 rows only;
+ 
+ --Joining two tables (Account as acc and Transaction as trans) using Inner Join.
+ 
+ SELECT acc.accountNumber,trans.transactionCode,trans.amount, trans.tranId,trans.debitCreditIndicator, trans.TraceNumber FROM Account acc INNER JOIN TransactionInfo trans ON acc.accountNumber=? and trans.AccountNo=?
