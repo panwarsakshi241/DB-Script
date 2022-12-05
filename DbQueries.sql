6728f0ce190089cde66f03a0c58f676c7299c7f3
sakshi panwar <sakshipanwar1912@gmail.com>
2022-12-05 18:45:36 +0530

    postgres queries

--- DbQueries.sql
+++ DbQueries.sql
@@ -5,3 +5,11 @@
  --Joining two tables (Account as acc and Transaction as trans) using Inner Join.
  
  SELECT acc.accountNumber,trans.transactionCode,trans.amount, trans.tranId,trans.debitCreditIndicator, trans.TraceNumber FROM Account acc INNER JOIN TransactionInfo trans ON acc.accountNumber=? and trans.AccountNo=?
+
+------Postgres queries.
+
+SELECT * FROM "ITransactionInfo" where "TranId"='3abb7ad1650011ed9b06000d3ac4a164';
+
+SELECT * FROM "IWireAdviceDetails" where "AdviceReference"='3aa708f5650011ed9a48000d3ac4a164'
+
+SELECT trans."AccountNo", trans."TranId", wires."AdviceReference" FROM "ITransactionInfo" as "trans" INNER JOIN "IWireAdviceDetails" as "wires" ON trans."AdviceReference" = wires."AdviceReference"
